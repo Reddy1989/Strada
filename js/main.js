29c00a52f87b626fdf67e4b65cfb3e42764d1f6b
Reddy1989 <online1989@mail.ru>
2023-06-17 00:21:54 +0300

    5 days type of data and variables

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-alert("hello, world from file");
+// alert("hello, world from file"); 
+
+let userName = 'alex';
+let userHeight;
+// let ИмяПользователя <- неправильно названная переменная 
+userHeight = 173;
+console.log(userName + " " + userHeight);
+console.log(2 + null);
+console.log(+null);
+console.log(`string + ${1 + 3}`)
+console.log(true, false);
+let a = 1;
+b = a;
+console.log(b);
+
+const MyAge = 34;
+// MyAge = 30;
+console.log(MyAge);
+
