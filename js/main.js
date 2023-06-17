730ae30a9b3d410de7319531149de351230d3266
Reddy1989 <online1989@mail.ru>
2023-06-17 16:47:16 +0300

    type conversion

--- js/main.js
+++ js/main.js
@@ -1,19 +1,33 @@
-// alert("hello, world from file"); 
+// // alert("hello, world from file"); 
 
-let userName = 'alex';
-let userHeight;
-// let ИмяПользователя <- неправильно названная переменная 
-userHeight = 173;
-console.log(userName + " " + userHeight);
-console.log(2 + null);
-console.log(+null);
-console.log(`string + ${1 + 3}`)
-console.log(true, false);
-let a = 1;
-b = a;
-console.log(b);
+// let userName = 'alex';
+// let userHeight;
+// // let ИмяПользователя <- неправильно названная переменная 
+// userHeight = 173;
+// console.log(userName + " " + userHeight);
+// console.log(2 + null);
+// console.log(+null);
+// console.log(`string + ${1 + 3}`)
+// console.log(true, false);
+// let a = 1;
+// b = a;
+// console.log(b);
 
-const MyAge = 34;
-// MyAge = 30;
-console.log(MyAge);
+// const MyAge = 34;
+// // MyAge = 30;
+// console.log(MyAge);
 
+let NewNum = '6';
+let NewString = 6;
+let NewStringWithLetter = '2b';
+
+
+console.log(NewNum + NewString)
+console.log(NewNum - NewString)
+console.log(NewNum / NewString)
+console.log(NewNum * NewString)
+
+console.log('-------')
+
+console.log(Number(NewNum) + Number(NewString));
+console.log(Number(NewNum) + Number(NewStringWithLetter));
