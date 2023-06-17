0cfa7a509f511680a61e91572e45409f402b236a
Reddy1989 <online1989@mail.ru>
2023-06-18 02:14:47 +0300

    day 6

--- js/main.js
+++ js/main.js
@@ -19,15 +19,48 @@
 
 let NewNum = '6';
 let NewString = 6;
+let NewBoolean = true;
 let NewStringWithLetter = '2b';
+const FIRSTNAME = 'Alexander';
+
+console.log('Базовые операторы "умножение"');
+
+console.log(NewNum * NewString);
 
 
-console.log(NewNum + NewString)
-console.log(NewNum - NewString)
-console.log(NewNum / NewString)
-console.log(NewNum * NewString)
 
-console.log('-------')
+console.log(NewNum + NewString);
+console.log(NewNum - NewString);
+console.log(NewNum / NewString);
+
+console.log('Проверка на тип');
+
+console.log(typeof NewNum);
+console.log(typeof NewString);
+console.log(typeof NewBoolean);
+console.log(typeof FIRSTNAME);
+
+
+console.log('Явное преведние типов');
 
 console.log(Number(NewNum) + Number(NewString));
 console.log(Number(NewNum) + Number(NewStringWithLetter));
+
+console.log('Остаток');
+
+console.log(533 % 2);
+
+console.log('Возведение в степень');
+
+console.log(2 ** 4);
+
+console.log('Инкремент и декремент');
+
+let IncrementedCounter = 4;
+let DecrementedCounter = 4;
+
+IncrementedCounter++
+DecrementedCounter--
+
+console.log(IncrementedCounter);
+console.log(DecrementedCounter);
