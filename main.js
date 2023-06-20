ced89f958b6562f30659f1b9e01971864ed33a53
Reddy1989 <online1989@mail.ru>
2023-06-20 13:52:27 +0300

    7 day task

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const u = undefined;
 
-const n = null;
+const n =  "a".match("b");
 
 const b = 5 % 2;
 Boolean(b);
 const nan = 4 * 'b';
 
+
 console.log(u, n, Boolean(b), nan)
 
 
+
+
 // Должно вывести в консоль: `undefined null true NaN`
+
